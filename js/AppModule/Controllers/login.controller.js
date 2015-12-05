2397d13db44eeb9aebbfded25ffbd3cbfc9d85e3
AndreasZeissner <zeissnerandreas@googlemail.com>
2015-12-05 16:37:46 +0100

    Removing Login Controller from Service adding it to AppModule

---
+++ js/AppModule/Controllers/login.controller.js
@@ -0,0 +1,57 @@
+/**
+ * Created by WangoeWoe on 05.12.15.
+ */
+/**
+ * Created by WangoeWoe on 03.12.15.
+ */
+(function () {
+  "use strict";
+
+  angular
+    .module('AppModule')
+    .controller('LoginControllerJP', login);
+
+  function login (LoginServiceREF) {
+    var vm = this;
+
+    vm.login = login;
+    vm.signup = signup;
+    vm.credentials = {
+      username: null,
+      password: null,
+      email: null,
+      forname: null,
+      surname: null,
+      mobilephone: null
+    }
+
+    function signup () {
+      /*
+       * call api
+       *   login -> POST Signup
+       *   and validate input
+       * */
+      if(  vm.credentials.username === null ||  vm.credentials.password === null ) {
+        console.log("no username or password");
+      } else if (  vm.credentials.email === null ||  vm.credentials.mobilephone === null ) {
+        console.log("Email or mobile Phone not set");
+      } else if ( vm.credentials.forname === null ||  vm.credentials.surname === null ) {
+        console.log("Forname or Surname not set");
+      } else {
+        // call for signup api !
+      }
+    }
+    function login () {
+      /*
+       * call api
+       *   login
+       *   and validate input
+       * */
+      if (vm.credentials.username === null || vm.credentials.password === null) {
+        console.log("No Credentials")
+      } else {
+        LoginServiceREF.checkLogin(vm.credentials.username, vm.credentials.password);
+      }
+    }
+  }
+})();
